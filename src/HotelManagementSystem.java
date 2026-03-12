e6454cbab1417f30a692ceb2d3deb74171c7e534
your GitHub username <your GitHub email>
2026-03-12 09:51:31 +0530

    UC5:Booking Request

--- src/HotelManagementSystem.java
+++ src/HotelManagementSystem.java
@@ -1,39 +1,43 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class RoomInventory {
+class Reservation {
 
-    private Map<String, Integer> roomAvailability;
+    private String guestName;
+    private String roomType;
 
-    public RoomInventory() {
-        roomAvailability = new HashMap<>();
-        roomAvailability.put("Single", 5);
-        roomAvailability.put("Double", 3);
-        roomAvailability.put("Suite", 2);
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
     }
 
-    public Map<String, Integer> getRoomAvailability() {
-        return roomAvailability;
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
     }
 }
 
-class RoomSearchService {
+class BookingRequestQueue {
 
-    public void searchAvailableRooms(RoomInventory inventory) {
+    private Queue<Reservation> requestQueue;
 
-        Map<String, Integer> availability = inventory.getRoomAvailability();
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
 
-        if (availability.get("Single") > 0) {
-            System.out.println("Single Room Available: " + availability.get("Single"));
-        }
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
 
-        if (availability.get("Double") > 0) {
-            System.out.println("Double Room Available: " + availability.get("Double"));
-        }
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
 
-        if (availability.get("Suite") > 0) {
-            System.out.println("Suite Room Available: " + availability.get("Suite"));
-        }
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
     }
 }
 
@@ -41,12 +45,24 @@ public class HotelManagementSystem {
 
     public static void main(String[] args) {
 
-        RoomInventory inventory = new RoomInventory();
+        System.out.println("Booking Request Queue");
 
-        RoomSearchService searchService = new RoomSearchService();
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
 
-        System.out.println("Available Rooms:");
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
 
-        searchService.searchAvailableRooms(inventory);
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        while (bookingQueue.hasPendingRequests()) {
+            Reservation r = bookingQueue.getNextRequest();
+            System.out.println(
+                    "Processing booking for " + r.getGuestName() +
+                            " - Room Type: " + r.getRoomType()
+            );
+        }
     }
 }
