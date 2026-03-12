168703afa496c0ecea1870da10e0df87572c9e90
your GitHub username <your GitHub email>
2026-03-12 09:44:52 +0530

    UC1: Add welcome message module

--- src/HotelManagementSystem.java
+++ src/HotelManagementSystem.java
@@ -1,5 +1,10 @@
 public class HotelManagementSystem {
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        System.out.println("==============================================");
+        System.out.println("     Welcome to Hotel Booking Management System");
+        System.out.println("==============================================");
+        System.out.println("System started successfully.");
     }
+
 }
